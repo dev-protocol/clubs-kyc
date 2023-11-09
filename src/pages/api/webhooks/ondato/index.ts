1b3852e9e77397f31957ce920ffd59d26deec5a7
aggre <hiroyuki.aggre@gmail.com>
2023-11-09 14:05:31 +0900

    fix the build error and reduce nesting

--- src/pages/api/webhooks/ondato/index.ts
+++ src/pages/api/webhooks/ondato/index.ts
@@ -1,11 +1,16 @@
 import type { APIRoute } from 'astro'
 import { json } from 'utils/json'
 import { headers } from 'utils/headers'
-import { whenDefinedAll, whenNotErrorAll } from '@devprotocol/util-ts'
+import {
+	whenDefinedAll,
+	whenNotError,
+	whenNotErrorAll,
+} from '@devprotocol/util-ts'
 import type { ReadonlyDeep } from 'type-fest'
 import { auth } from 'utils/auth'
 import { redis } from 'utils/db'
 import { v4 as uuidv4 } from 'uuid'
+import { always } from 'ramda'
 
 type RequestBody = ReadonlyDeep<{
 	payload?: {
@@ -24,57 +29,59 @@ export const POST: APIRoute = async ({ request, clientAddress }) => {
 		.then((x) => x as RequestBody)
 		.catch((err) => new Error(err))
 
-	return (
-		whenNotErrorAll(
-			[auth(request), await redis(), body],
-			([, client, data]) =>
-				whenDefinedAll(
-					[data.payload?.status, data.payload?.identityVerificationId],
-					async ([status, idv]) => {
-						const data = {
-							status,
-							idv,
-						}
+	const isValidRequest = auth(request)
+		? true
+		: new Error('Authentication failed')
 
-						/**
-						 * gererate a unique key for the record
-						 */
-						const recordKey = uuidv4()
+	const db = await redis()
 
-						/**
-						 * set the record data in redis
-						 */
-						// eslint-disable-next-line functional/no-expression-statements
-						await client.hSet(recordKey, data)
+	const props = whenNotError(
+		body,
+		(data) =>
+			whenDefinedAll(
+				[data.payload?.status, data.payload?.identityVerificationId],
+				([status, idv]) => ({
+					status,
+					idv,
+				}),
+			) ?? new Error('Payload params undefined'),
+	)
 
-						/**
-						 * set the index in redis
-						 */
-						// eslint-disable-next-line functional/no-expression-statements
-						await client.hSet('index:ondatoVerificationId', data.idv, recordKey)
-						// eslint-disable-next-line functional/no-expression-statements
-						await client.quit()
+	/**
+	 * gererate a unique key for the record
+	 */
+	const recordKey = uuidv4()
 
-						return new Response(
-							json({ data: null, message: 'Authentication failed' }),
-							{ status: 200, headers },
-						)
-					},
-				) ??
-				new Response(
-					json({ data: null, message: 'Payload params undefined' }),
-					{
-						status: 401,
-						headers,
-					},
-				),
-		) ??
-		new Response(
-			json({ data: null, message: 'Auth or redis or body parsing failed' }),
-			{
+	const result = await whenNotErrorAll(
+		[props, db, isValidRequest],
+		async ([data, client]) => {
+			/**
+			 * set the record data in redis
+			 */
+			const setRecord = await client
+				.hSet(recordKey, data)
+				.catch((err) => new Error(err))
+
+			/**
+			 * set the index in redis
+			 */
+			const setIndex = await whenNotError(
+				setRecord,
+				always(client.hSet('index:ondatoVerificationId', data.idv, recordKey)),
+			)
+			const quit = await client.quit().catch((err) => new Error(err))
+
+			return whenNotErrorAll([setIndex, quit], always(true))
+		},
+	)
+
+	return result instanceof Error
+		? new Response(json({ data: null, message: result.message }), {
 				status: 401,
 				headers,
-			},
-		)
-	)
+		  })
+		: new Response(json({ data: null, message: 'Success' }), {
+				status: 200,
+				headers,
+		  })
 }
